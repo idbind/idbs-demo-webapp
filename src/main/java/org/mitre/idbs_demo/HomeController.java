4f925b53b681b4c03c86b69627b44f1a962c1082
Chris Berger <cberger8@yahoo.com>
2015-07-14 09:52:35 -0400

    Added initial 'hello world' files

---
+++ src/main/java/org/mitre/idbs_demo/HomeController.java
@@ -0,0 +1,14 @@
+package org.mitre.idbs_demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HomeController {
+	
+	/*@RequestMapping(value = "/")
+	public ModelAndView homeView() {
+		return new ModelAndView("home");
+	}*/
+}
---
+++ src/main/java/org/mitre/idbs_demo/WebConfig.java
@@ -0,0 +1,17 @@
+package org.mitre.idbs_demo;
+
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcAutoConfigurationAdapter {
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+	}
+}
