836339858793a8f6136f525940829e0c12939475
Chris Berger <cberger8@yahoo.com>
2015-08-02 22:57:41 -0400

    Moved remaining hardcoded values to properties file

--- src/main/java/org/mitre/idbs_demo/web/HomeController.java
+++ src/main/java/org/mitre/idbs_demo/web/HomeController.java
@@ -5,6 +5,7 @@ import org.mitre.idbs_demo.model.TokenResponse;
 import org.mitre.idbs_demo.service.AuthTokenService;
 import org.mitre.idbs_demo.service.BoundIdentityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,24 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class HomeController {
 	
+	@Value( "${client.id}" )
+	private String clientId;
+	
+	@Value( "${client.secret}" )
+	private String clientSecret;
+	
+	@Value( "${client.grantType}" )
+	private String clientGrant;
+	
+	@Value( "${client.scope}" )
+	private String clientScope;
+	
+	@Value( "${idbsQuery.issuer}" )
+	private String issuer;
+	
+	@Value( "${idbsQuery.subject}" )
+	private String subject;
+	
 	@Autowired
 	private AuthTokenService tokenService;
 	
@@ -26,12 +45,12 @@ public class HomeController {
 	public TokenResponse getAuthorizationToken() {
 		
 		//TODO: Where do these arguments come from?
-		return tokenService.getAuthToken("client", "secret", "client_credentials", "org.mitre.idbind.query");
+		return tokenService.getAuthToken(clientId, clientSecret, clientGrant, clientScope);
 	}
 	
 	@RequestMapping(value = "/getIdentities", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Identity[] getBoundIdentities() {
 		
-		return identityService.getIdentities("http://localhost:8080/openid-connect-server-webapp/", "90342.ASDFJWFA");
+		return identityService.getIdentities(issuer, subject);
 	}
 }
